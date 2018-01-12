381c4d16484d13ef74af55529fd052521e441926
John Polhill <JohnPolhill42@gmail.com>
2018-01-11 20:04:29 -0600

    Add Lazer
    Add Lazer

--- GameEngine/src/com/engine/galaga/Lazer.java
+++ GameEngine/src/com/engine/galaga/Lazer.java
@@ -1,4 +1,28 @@
 package com.engine.galaga;
 
+import com.engine.framework.display.Render;
+import com.engine.framework.math.Rectangle;
+
+import java.awt.*;
+
 public class Lazer {
+
+    static final Color COLOR = Color.red;
+
+    //Instance Variables
+    float speed;
+    Rectangle body;
+
+    public Lazer(float s, Rectangle b) {
+        speed = s;
+        body = b;
+    }
+
+    public void update() {
+        body.y -= speed;
+    }
+
+    public void draw(Render r) {
+        r.drawRectangle(body, COLOR);
+    }
 }
--- GameEngine/src/com/engine/galaga/Ship.java
+++ GameEngine/src/com/engine/galaga/Ship.java
@@ -1,27 +1,31 @@
 package com.engine.galaga;
 
+import com.engine.framework.Input;
 import com.engine.framework.display.Render;
 import com.engine.framework.display.Texture;
 import com.engine.framework.math.Rectangle;
-import com.engine.framework.math.Vector2;
 
 import java.util.ArrayList;
 
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_RIGHT;
+
 public class Ship {
 
-    Vector2 velocity;
     Rectangle body;
     float speed;
     int hp;
 
     ArrayList<Lazer> firedLazers;
+
+    //Sprite Variables
     static Texture texture;
     Rectangle[] src;
     int imageIndex = 0;
     int imageSpeed = 2;
+    int imageCounter = imageSpeed;
 
     public Ship(Rectangle b, float s, int hp) {
-        velocity = new Vector2();
         body = b;
         speed = s;
         this.hp = hp;
@@ -34,15 +38,32 @@ public class Ship {
     }
 
     public void update() {
-
+        animate();
+        move();
     }
 
     public void draw(Render r) {
         r.drawImage(body, src[imageIndex], texture);
     }
 
-    private void move() {
+    private void animate() {
+        if(imageCounter <= 0) {
+            imageCounter = imageSpeed;
+            imageIndex = imageIndex == 0 ? 1 : 0;
+        }
+        else {
+            imageCounter--;
+        }
+    }
 
+    private void move() {
+        if (Input.keys[GLFW_KEY_LEFT]) {
+            body.x -= speed;
+        }
+        else if (Input.keys[GLFW_KEY_RIGHT]) {
+            body.x += speed;
+        }
+        body.clamp(0, 0, Galaga.WIDTH, Galaga.HEIGHT);
     }
 
     private void srcRects() {
